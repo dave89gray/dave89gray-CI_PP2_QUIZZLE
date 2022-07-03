e87c7c648bf2bf92e735845d0e8a5a0bc3ff43cd
dave89gray <david_gray89@aol.com>
2022-07-03 13:41:20 +0000

    Layout some javascript items and functions that may be needed

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * This function will reset the game
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    beginGame();
+});
+
+/**
+ * Create User answer area, 5 attempts in the array with 4 letters.
+ */
+
+/**
+ * Create a function that will create each row of user answer and each letter guess
+ */
+
+/**
+ * Create a keyboard letters using an Array that can be indexed through
+ */
+
+/**
+ * Create Keyboard buttons using document.createElement()
+ */
+
+/**
+ * Function for keyboard tile to be clicked
+ */
