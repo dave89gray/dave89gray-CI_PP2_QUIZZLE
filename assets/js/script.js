647ddc4ec0698b235c9ee589b20d3ac96c77031b
dave89gray <david_gray89@aol.com>
2022-07-04 10:16:47 +0000

    Add style to keyboard keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,38 @@
 /**
  * This function will reset the game and load the keyboard
-
+*/
 document.addEventListener("DOMContentLoaded", function () {
-    /** 
-    const keyboard = document.querySelector('.keyboard-box');
+    const keyboard = document.querySelector(".keyboard-box");
+
+
+/**
+ * Create keyboard letters using an Array
+ */
     const letters = [
-        'Q','W','E','R','T','Y','U','I','O',
-        'P','A','S','D','F','G','H','J','K','L',
+        'Q','W','E','R','T','Y','U','I','O','P',
+        'A','S','D','F','G','H','J','K','L',
         'ENTER','Z','X','C','V','B','N','M','«',
-    ]
-    
+    ];
+
+/**
+ * Create Keyboard buttons using document.createElement()
+ */
     letters.forEach(letter => {
         const buttonElement = document.createElement('button');
         buttonElement.textContent = letter
         buttonElement.setAttribute('id', letter);
-        buttonElement.addEventListener('click', handleClick(letter));
+        buttonElement.addEventListener('click', handleClick);
         keyboard.append(buttonElement);
-    })
-
+    });
 });
-    */
+
+/**
+ * Function for keyboard tile to be clicked
+ * with an if loop in there
+ */
+const handleClick = () => {
+    console.log('clicked!');
+}
 /**
  * Create User answer area, 5 attempts in the array with 4 letters.
  */
@@ -28,38 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
  * Create a function that will create each row of user answer and each letter guess
  */
 
-/**
- * Create a keyboard letters using an Array that can be indexed through
- */
- const keyboard = document.querySelector(".keyboard-box");
- const letters = [
-    'Q','W','E','R','T','Y','U','I','O','P',
-    'A','S','D','F','G','H','J','K','L',
-    'ENTER','Z','X','C','V','B','N','M','«',
-];
 
-letters.forEach(letter => {
-    const buttonElement = document.createElement('button');
-    buttonElement.textContent = letter
-    keyboard.append(buttonElement);
-});
-
-/**
- * Create Keyboard buttons using document.createElement()
- *  */
-
-
-
-
-/**
- * Function for keyboard tile to be clicked
- * with an if loop in there
-
-
-function handleClick () {
-    console.log();
-}
- */
 
 /**
  * Function to add colour to a key
