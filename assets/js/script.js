10513299bbef9aba1b949c52b0d47aa9a8602cac
dave89gray <david_gray89@aol.com>
2022-07-04 09:46:34 +0000

    Add basic keyboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
 /**
- * This function will reset the game
- */
-document.addEventListener("DOMContentLoaded", function () {
-    beginGame();
-});
+ * This function will reset the game and load the keyboard
 
+document.addEventListener("DOMContentLoaded", function () {
+    /** 
+    const keyboard = document.querySelector('.keyboard-box');
+    const letters = [
+        'Q','W','E','R','T','Y','U','I','O',
+        'P','A','S','D','F','G','H','J','K','L',
+        'ENTER','Z','X','C','V','B','N','M','«',
+    ]
+    
+    letters.forEach(letter => {
+        const buttonElement = document.createElement('button');
+        buttonElement.textContent = letter
+        buttonElement.setAttribute('id', letter);
+        buttonElement.addEventListener('click', handleClick(letter));
+        keyboard.append(buttonElement);
+    })
+
+});
+    */
 /**
  * Create User answer area, 5 attempts in the array with 4 letters.
  */
@@ -16,21 +31,40 @@ document.addEventListener("DOMContentLoaded", function () {
 /**
  * Create a keyboard letters using an Array that can be indexed through
  */
+ const keyboard = document.querySelector(".keyboard-box");
  const letters = [
-    'Q','W','E','R','T','Y','U','I','O',
-    'P','A','S','D','F','G','H','J','K','L',
+    'Q','W','E','R','T','Y','U','I','O','P',
+    'A','S','D','F','G','H','J','K','L',
     'ENTER','Z','X','C','V','B','N','M','«',
-]
+];
+
+letters.forEach(letter => {
+    const buttonElement = document.createElement('button');
+    buttonElement.textContent = letter
+    keyboard.append(buttonElement);
+});
 
 /**
  * Create Keyboard buttons using document.createElement()
- */
-letters.forEach(letter => {
-    const keyElement = document.createElement('key')
-    keyElement.textContent = letter
-    KeyboardEvent.append(keyElement)
-})
+ *  */
+
+
+
 
 /**
  * Function for keyboard tile to be clicked
+ * with an if loop in there
+
+
+function handleClick () {
+    console.log();
+}
+ */
+
+/**
+ * Function to add colour to a key
+ */
+
+/**
+ * Function to flip tile
  */
