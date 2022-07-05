50dfc8420085e747f6ae9bf8bd6a6b51a64f1eef
dave89gray <david_gray89@aol.com>
2022-07-05 14:53:20 +0000

    Change structure of javascript and place within playGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,63 @@
-(function () {
+/**
+ * This function will reset the game and load the keyboard,
+ * tiles for user guesses and words to be chosen from
+ */
+document.addEventListener("DOMContentLoaded", function () {
+
+});
+
+function playGame() {
+
+    // Get the div from HTMl with the class of "keyboard-box"
+    let keyboard = document.querySelector(".keyboard-box");
+
+    // Create keyboard letters using an Array
+    let letters = [
+        'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+        'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+        'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '«',
+    ];
+
+    /**
+     * Create Keyboard buttons using document.createElement() and assign
+     * a letter from the letters array
+     */
+    letters.forEach(letter => {
+        let buttonElement = document.createElement('button');
+        buttonElement.textContent = letter
+        buttonElement.setAttribute('id', letter);
+        buttonElement.addEventListener('click', () => handleMouseClick(letter));
+        keyboard.append(buttonElement);
+    });
+
+    // Get the div from HTMl with the class of "tiles-box"
+    let tileBox = document.querySelector(".tiles-box");
+
+    // Create Array for tiles for the user input
+    let userInputs = [
+        ["", "", "", ""],
+        ["", "", "", ""],
+        ["", "", "", ""],
+        ["", "", "", ""],
+        ["", "", "", ""],
+    ]
+
+    // Create tile rows using the same method as the Keyboard
+    userInputs.forEach((userInput, userInputIndex) => {
+        let attemptRowElement = document.createElement('div');
+        attemptRowElement.setAttribute('id', 'userInput-' + userInputIndex);
+
+        /**
+         * Create individual tile and append it to the row created above
+         */
+        userInput.forEach((attempt, attemptIndex) => {
+            let tileElement = document.createElement('div');
+            tileElement.setAttribute('id', 'userInput-' + userInputIndex + '-tile-' + attemptIndex);
+            tileElement.classList.add('tile');
+            attemptRowElement.append(tileElement);
+        })
+        tileBox.append(attemptRowElement);
+    })
 
     // Default starting row
     let startingRow = 0;
@@ -6,71 +65,15 @@
     // Default starting tile
     let currentTile = 0;
 
-    /**
-     * This function will reset the game and load the keyboard,
-     * tiles for user guesses and words to be chosen from
-     */
-    document.addEventListener("DOMContentLoaded", function () {
+    // Array of words to be selected from
+    const words = [
+        'BEST', 'BORE', 'GOOD', 'GIMP', 'NOTE', 'SEER', 'PORK', 'BOLT', 'TUBE', 'EPIC', 'FORE', 'BASS', 'HUSH',
+        'JUST', 'PART', 'PASS', 'BLUE', 'PINK', 'DRAG', 'MIND', 'PART', 'GLOW', 'URGE', 'DOOR', 'BUST', 'FAKE',
+        'DART', 'VIEW', 'SCUD', 'HOLD', 'WHIZ', 'FROG', 'BIKE', 'VASE', 'MANY', 'BOAT', 'NEAR', 'LONE', 'SPIN',
+        'GOAT', 'GOAL', 'YAWN', 'FIVE', 'DARN', 'BEER', 'RICH', 'BALM', 'COAX', 'PIPE', 'TWIG', 'TOUR', 'FAIR',
+    ];
 
-        // Get the div from HTMl with the class of "keyboard-box"
-        let keyboard = document.querySelector(".keyboard-box");
-
-        // Create keyboard letters using an Array
-        let letters = [
-            'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
-            'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-            'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '«',
-        ];
-
-        // Array of words to be selected from
-        let wordle = [
-            'BEST', 'BORE', 'GOOD', 'GIMP', 'NOTE', 'SEER', 'PORK', 'BOLT', 'TUBE', 'EPIC', 'FORE', 'BASS', 'HUSH',
-            'JUST', 'PART', 'PASS', 'BLUE', 'PINK', 'DRAG', 'MIND', 'PART', 'GLOW', 'URGE', 'DOOR', 'BUST', 'FAKE',
-            'DART', 'VIEW', 'SCUD', 'HOLD', 'WHIZ', 'FROG', 'BIKE', 'VASE', 'MANY', 'BOAT', 'NEAR', 'LONE', 'SPIN',
-            'GOAT', 'GOAL', 'YAWN', 'FIVE', 'DARN', 'BEER', 'RICH', 'BALM', 'COAX', 'PIPE', 'TWIG', 'TOUR', 'FAIR',
-        ];
-
-        /**
-         * Create Keyboard buttons using document.createElement() and assign
-         * a letter from the letters array
-         */
-        letters.forEach(letter => {
-            let buttonElement = document.createElement('button');
-            buttonElement.textContent = letter
-            buttonElement.setAttribute('id', letter);
-            buttonElement.addEventListener('click', () => handleMouseClick(letter));
-            keyboard.append(buttonElement);
-        });
-
-        // Get the div from HTMl with the class of "tiles-box"
-        let tileBox = document.querySelector(".tiles-box");
-
-        // Create Array for tiles for the user input
-        let userInputs = [
-            ["", "", "", ""],
-            ["", "", "", ""],
-            ["", "", "", ""],
-            ["", "", "", ""],
-            ["", "", "", ""],
-        ]
-
-        // Create tile rows using the same method as the Keyboard
-        userInputs.forEach((userInput, userInputIndex) => {
-            let attemptRowElement = document.createElement('div');
-            attemptRowElement.setAttribute('id', 'userInput-' + userInputIndex);
-
-            /**
-             * Create individual tile and append it to the row created above
-             */
-            userInput.forEach((_attempt, attemptIndex) => {
-                let tileElement = document.createElement('div');
-                tileElement.setAttribute('id', 'userInput-' + userInputIndex + '-tile-' + attemptIndex);
-                tileElement.classList.add('tile');
-                attemptRowElement.append(tileElement);
-            })
-            tileBox.append(attemptRowElement);
-        })
-    });
+    let correctWord = words[Math.floor(Math.random() * words.length)];
 
     /**
      * Function to add a letter to the tile row
@@ -102,6 +105,14 @@
         }
     }
 
+    let checkRow = () => {
+
+        if (currentTile === 5) {
+            let attempt = userInputs[startingRow].join('');
+            console.log('guess is ' + attempt, 'wordle is ' + correctWord);
+        }
+    }
+
     /**
      * Function for keyboard tile to be clicked, taken from the EventListener above,
      * with an if loop in there
@@ -116,13 +127,13 @@
         }
 
         if (letter === 'ENTER') {
-            console.log('check row');
+            checkRow();
+            console.log('clicked' + correctWord);
             return
         }
 
         addLetter(letter);
     }
-
 
     /**
      * Function to add colour to a key
@@ -131,4 +142,7 @@
     /**
      * Function to flip tile
      */
-})();
+
+}
+
+playGame();
