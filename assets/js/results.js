0547d3a05528bc47986155f6129b6a9c4e7b37b6
dave89gray <david_gray89@aol.com>
2022-07-07 13:16:32 +0000

    Add start of results page Javascript

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,10 @@
+let username = document.getElementById('username');
+
+//log the username when a key returns up
+username.addEventListener('keyup', () => {
+    console.log(username.value);
+});
+
 saveScore = (e) => {
     console.log("Click the save button");
     // Prevent the form taking default action of posting to a different page
