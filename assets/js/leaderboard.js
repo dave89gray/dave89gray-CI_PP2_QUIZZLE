1921c5c5ef56c9e2ca4333bb450471d579db314b
dave89gray <david_gray89@aol.com>
2022-07-08 12:04:47 +0000

    Change Javascript to try and get leaderboard to populate

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,14 +1,18 @@
 let finalLeaderboard = document.getElementById('finalLeaderboard');
-let scores = JSON.parse(localStorage.getItem('scores')) || [];
+let leaderboardStyle = JSON.parse(localStorage.getItem('leaderboardStyle')) || [];
+
 
 /**
- * This will retrieve the users scores and add them in a list
- */
-finalLeaderboard.innerHTML = scores
+ * This will retrieve the users scores and add them as a list element
+ * within the <UL>
+*/
+ finalLeaderboard.innerHTML = leaderboardStyle
 
-//Map used to take the array of items and convert to a string
-.map(score => {
-    // Return the string version of our required info
-    return `<li class = "scoresTable>${score.name} = ${score.score}</li>`;
-})
-.join("");
+    //Map used to take the array of items and convert to a string
+    leaderboardStyle.map(score => {
+        console.log(`${score.username}-${score.score}`);
+        // Return the string version of our required info
+        //return `<li class="scoresTable>${score.name} - ${score.score}</li>`;
+    })
+    
+    //.join(""); 
