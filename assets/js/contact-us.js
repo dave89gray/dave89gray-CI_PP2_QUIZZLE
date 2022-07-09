b2334ffe5318526654874454662defa423fafd0e
dave89gray <david_gray89@aol.com>
2022-07-09 14:01:07 +0000

    Add new return to home button on the Contact Us page

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -1,2 +1,7 @@
 let firstName = document.getElementById('fname');
 let lastName = document.getElementById('lname');
+let email = document.getElementById('email');
+
+console.log(firstName.value);
+console.log(lastName.value);
+console.log(email.value);
