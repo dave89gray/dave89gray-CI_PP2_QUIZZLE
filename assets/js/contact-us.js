56163d3f9e2f8370f50913101bbd2c40a989c6e9
dave89gray <david_gray89@aol.com>
2022-07-09 14:12:08 +0000

    Add initial JS for the contact form

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -5,3 +5,13 @@ let email = document.getElementById('email');
 console.log(firstName.value);
 console.log(lastName.value);
 console.log(email.value);
+
+/**
+ * This will send the data collected from the form submission to email after the user
+ * has clicked submit. It should return a Thank You message.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('form').addEventListener('submit', function (event) {
+        
+    })
+})
