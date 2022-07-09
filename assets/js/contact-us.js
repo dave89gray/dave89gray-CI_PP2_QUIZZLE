05739f869739313c12085e30cb55d6cd3951080f
dave89gray <david_gray89@aol.com>
2022-07-09 18:36:54 +0000

    Re-add the JS code and the thankYouMessage function

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -6,7 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('form').addEventListener('submit', function (event) {
         // Prevent the form taking default action of posting to a different page
         event.preventDefault();
-        emailjs.init(GdX0myu7Q38VyMRFe);
-        emailjs.sendForm(quizzle, )
-    })
-})
+        emailjs.init('user_GdX0myu7Q38VyMRFe');
+        emailjs.sendForm('quizzle', 'contact_form', this)
+            .then(function() {
+                //Email successfully sent
+            }, function (error) {
+                //Email sumbission unsuccessful
+                console.log('FAILED...', error);
+            });
+            thankYouMessage();
+    });
+});
+
+/**
+ * This will replace the contents of the contact us page with the below HTML
+ */
+function thankYouMessage() {
+    let html = `
+    <h4>Thank you for leaving us your feedback</h4>
+    <div class="feedback_container flex-center flex-column">
+        <img src="assets/images/feedback_img.jpeg" alt="The Royal Banner of Scotland flag">
+    </div>
+    <div>
+        <a href="index.html" class="home-button">return to Home!</a>
+    </div>`;
+    document.getElementsByClassName("contact-container").innerHTML = html;
+}
